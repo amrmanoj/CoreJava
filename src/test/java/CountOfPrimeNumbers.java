ef0aa5d331abc50e179cd11ddc0161bcdd05a8ad
amrmanoj <amr.manojkumarreddy.com>
2023-03-16 07:37:47 +0530

    changes 0737

--- src/test/java/CountOfPrimeNumbers.java
+++ src/test/java/CountOfPrimeNumbers.java
@@ -4,7 +4,7 @@ public class CountOfPrimeNumbers
 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Lower Number");
+        System.out.println("Enter Lower Number");
         int x= Integer.parseInt(scanner.nextLine());
         System.out.println("Enter Upper Number");
         int y= Integer.parseInt(scanner.nextLine());
